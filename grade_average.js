c4279c0ff4b4be1cca7b01382039e9518a83471c
Aunuxx <132584420+Aunuxx@users.noreply.github.com>
2024-04-01 12:35:47 -0600

    Update grade_average.js

--- grade_average.js
+++ grade_average.js
@@ -1,11 +1,22 @@
 const path = "#content div.objective div.objective-assessments div.assessment-score span"
 let count = 0;
 let total = 0;
+const score_path = "#mastery_level_chart > h2"
+
 window.addEventListener('load', function () {
 
   for (let i of document.querySelectorAll(path)) {
-    count += 1
-    total += parseInt(i.textContent[0])
+    count += 1;
+    total += parseInt(i.textContent[0]);
   };
-  console.log(total/(count))
+
+  // sbl score
+  let score = (total/count).toFixed(2);
+  document.querySelector(score_path).innerHTML = score
+
+  document.querySelector(score_path).style.left = '15px'
+  document.querySelector(score_path).style.right = '35px'
+
+
 });
+
